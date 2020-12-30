c9365d4787f48c3154e980c6007e7db586069468
CtrlMj <43945314+CtrlMj@users.noreply.github.com>
2020-12-29 21:14:52 -0500

    Update model.py

--- model/model.py
+++ model/model.py
@@ -4,7 +4,7 @@ from keras.layers import Dense
 from keras.layers import Convolution2D, MaxPooling2D, Dropout, Flatten
 from keras.optimizers import Adam
 
-def nvidia():
+def nvidia(lr=0.005, dropouts = [0, 0, 0.3]):
   model = Sequential()
   #subsample defines the strides of the kernel by which it traverses
   model.add(Convolution2D(24, (5, 5), subsample=(2, 2), input_shape=(66, 200, 3), activation='elu'))
@@ -16,11 +16,11 @@ def nvidia():
   model.add(Convolution2D(64, (3, 3), activation='elu'))
   model.add(Flatten())
   model.add(Dense(100, activation='elu'))
-  model.add(Dropout(0.0))
+  model.add(Dropout(dropouts[0]))
   model.add(Dense(50,  activation='elu'))
-  model.add(Dropout(0.0))
+  model.add(Dropout(dropouts[1]))
   model.add(Dense(10,  activation='elu'))
-  model.add(Dropout(0.3))
+  model.add(Dropout(dropouts[2]))
   model.add(Dense(1))
   model.compile(optimizer=Adam(0.005), loss='mse', metrics=['accuracy'])
   return model
