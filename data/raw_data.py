ad27ae35ba9aa42cb7ff86aafdd6d4017ea3b3e2
CtrlMj <43945314+CtrlMj@users.noreply.github.com>
2020-12-29 20:49:34 -0500

    Update raw_data.py

--- data/raw_data.py
+++ data/raw_data.py
@@ -7,15 +7,14 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 
-data = pd.read_csv("SelfDriving/log.csv", names = ["center", "left", "right", "steering", "throttle", "reverse", "speed"])
-data['center'] = data['center'].apply(lambda x: ntpath.basename(x))
 
-def render_raw(path="SelfDriving/log.csv"):
+def render_raw(path="SelfDriving/log.csv", testsize=0.2, n_bins=25, threshold=300):
   data = pd.read_csv(path, names = ["center", "left", "right", "steering", "throttle", "reverse", "speed"])
   data['center'] = data['center'].apply(lambda x: ntpath.basename(x))
+  hist, bins = np.histogram(data['steering'], n_buckets)
   
   to_be_removed = []                                                           
-  for i in range(25):
+  for i in range(n_bins):
     lst = []                                                                     
     for j in range(len(data['steering'])):
         if data['steering'][j] <= bins[i+1] and data['steering'][j] >= bins[i]:
@@ -23,7 +22,8 @@ def render_raw(path="SelfDriving/log.csv"):
     lst = shuffle(lst)                                                            
     to_be_removed.extend(lst[threshold:])
   data.drop(data.index[to_be_removed], inplace=True)
+  
   imagepath = np.array(data['center'])
   steer = np.array(data['steering'])
-  X_train, X_test, y_train, y_test = train_test_split(imagepath, steer, test_size = 0.2, random_state = 1)
+  X_train, X_test, y_train, y_test = train_test_split(imagepath, steer, test_size = testsize, random_state = 1)
   return X_train, X_test, y_train, y_test
